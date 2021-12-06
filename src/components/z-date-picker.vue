f482c054e05c09d49b6d80c3c0017f88d0638151
chenzehao <chenzehao@sunengineering.cn>
2021-12-06 11:09:33 +0800

    新增z-button功能，日期选择器

---
+++ src/components/z-date-picker.vue
@@ -0,0 +1,37 @@
+<!--
+ - All rights reserved.
+ - @author Zeao
+ - Description: z-date 日期选择组件
+ - Changelog:
+ - Revision 1.0 2021-12-03  Zeao 
+ - - 初始化
+ -->
+<template>
+  <el-date-picker class='z-date-picker'
+                    :value='value'
+                    v-bind='$attrs'
+                    v-on='$listeners'
+    />
+</template>
+
+<script>
+export default {
+  name: 'z-date-picker',
+  props: {
+    value: null,
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {
+  },
+}
+</script>
+<style scoped lang='scss'>
+
+.z-date-picker {
+  height: 32px;
+  line-height: 32px;
+}
+</style>
