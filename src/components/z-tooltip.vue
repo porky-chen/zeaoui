47c75082bc58be50994fdb6c1ee0ad57b87472b6
chenzehao <chenzehao@sunengineering.cn>
2021-12-06 11:11:59 +0800

    新增z-tooltip组件，文字提示

---
+++ src/components/z-tooltip.vue
@@ -0,0 +1,30 @@
+<!--
+ - All rights reserved.
+ - @author Zeao
+ - Description: z-tooltip 文字提示
+ - Changelog:
+ - Revision 1.0 2021-12-03  Zeao 
+ - - 初始化
+ -->
+<template>
+  <div class='su-tooltip'>
+    <el-tooltip v-bind='$attrs' v-on='$listeners'>
+      <template v-for='name in Object.keys($slots)' v-slot:[name]>
+        <slot :name='name' />
+      </template>
+    </el-tooltip>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'z-tooltip',
+  props: {},
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {
+  },
+}
+</script>
